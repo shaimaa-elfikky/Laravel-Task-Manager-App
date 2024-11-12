04c05e70727945f23a674fe8d94ab2b2e574f9aa
shaimaa79 <shimo11shimo11@gmail.com>
2024-11-12 11:06:40 +0200

    44

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('tasks', 'TaskController@index');
-Route::post('tasks', 'TaskController@store');
-Route::get('tasks/{id}', 'TaskController@show');
-Route::get('tasks/{id}/edit', 'TaskController@edit');
-Route::put('tasks/{id}', 'TaskController@update');
-Route::delete('tasks/{id}', 'TaskController@destroy');
-
+Route::resource('tasks', 'TaskController');
 Route::resource('task-lists', 'TaskListController');
 
 Route::get('task-lists/{id}/share', 'TaskListController@share')->name('task-list.share');
